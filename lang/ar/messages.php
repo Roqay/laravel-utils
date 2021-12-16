5644460edc48da728ed9e3f2e59c3e647ff93ff0
M.Ismail <mismail@roqay.com.kw>
2021-12-16 10:07:47 +0200

    Add AR locales files

---
+++ lang/ar/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'execution_status' => [
+        'status_1' => 'قيد التنفيذ',
+        'status_2' => 'معلقة',
+        'status_3' => 'مغلقة',
+    ],
+
+    'responses' => [
+        'Wrong Credentials' => 'بيانات دخول خاطئة, يرجى التأكد من البريد الإلكتروني وكلمة المرور!',
+        'Updated Successfully' => 'تم التعديل بنجاح',
+        'Saved Successfully' => 'تم الحفظ بنجاح',
+        'Deleted Successfully' => 'تم الحذف بنجاح',
+        'Can not delete, model has related records' => 'لا يمكن الحذف. قد يؤدى الحذف لعطل فى أجزاء أخرى من النظام!',
+    ],
+
+];
